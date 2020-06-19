08e7b495910eee7e5e628532c3d21b5fab814e8f
EkremDincel <ekremdincel@gmail.com>
2020-06-19 15:44:33 +0300

    Update astcompiler.py
    Compiler sınıfı oluşturuldu.

--- astcompiler.py
+++ astcompiler.py
@@ -1,126 +1,118 @@
 operands = {"+":"add", "-":"sub", "/":"div", "*":"mul"}
 
-line_number = 0 # sınıf haline getir.
+__all__ = ["Compiler"]
 
 nodes = {}
 def node(f):
     nodes[f.__name__] = f
     return f
 
-def walk_node(ast):
-    node, *elements = ast
-    s = nodes[node](*elements)
-    return s
+class Compiler():
+    line_number = 0 # __init__
 
-compile_ast = walk_node # main funtion
-__all__ = ["comp_ast"]
+    __all__ = ["compile_ast"]
 
-@node
-def scope(e):
-    global line_number
-    line_number += 1
-    if not e: return ""
-    s = "scope 1\n"
-    for i in e:
-        s += walk_node(i)
-    line_number += 1
-    return s + "scope -1\n"
+    def walk_node(self, ast):
+        node, *elements = ast
+        s = nodes[node](self, *elements)
+        return s
 
-@node
-def pop(e):
-    global line_number
-    r = walk_node(e) + "pop\n"
-    line_number += 1
-    return r
+    compile_ast = walk_node # main funtion
+    
 
-@node
-def use(e):
-    global line_number
-    s = ""
-    for i in e:
-        s += "use " + i + "\n"
-        line_number += 1
-    return s
+    @node
+    def scope(self, e):
+        self.line_number += 1
+        if not e: return ""
+        s = "scope 1\n"
+        for i in e:
+            s += self.walk_node(i)
+        self.line_number += 1
+        return s + "scope -1\n"
 
-@node
-def useall(e):
-    global line_number
-    s = ""
-    for i in e:
-        s += "useall " + i + "\n"
-        line_number += 1
-    return s
+    @node
+    def pop(self, e):
+        r = self.walk_node(e) + "pop\n"
+        self.line_number += 1
+        return r
 
-@node
-def assign(name, expr):
-    global line_number
-    r = walk_node(expr) + "storename " + name + "\n"
-    line_number += 1
-    return r
+    @node
+    def use(self, e):
+        s = ""
+        for i in e:
+            s += "use " + i + "\n"
+            self.line_number += 1
+        return s
 
-@node
-def op(operand, a, b):
-    global line_number
-    r = walk_node(a) + walk_node(b) + "op " + operands[operand] + "\n"
-    line_number += 1
-    return r
+    @node
+    def useall(self, e):
+        s = ""
+        for i in e:
+            s += "useall " + i + "\n"
+            self.line_number += 1
+        return s
 
-@node
-def const(c):
-    global line_number
-    line_number += 1
-    return "loadconst " + c + "\n"
+    @node
+    def assign(self, name, expr):
+        r = self.walk_node(expr) + "storename " + name + "\n"
+        self.line_number += 1
+        return r
 
-@node
-def var(n):
-    global line_number
-    line_number += 1
-    return "loadname " + n + "\n"
+    @node
+    def op(self, operand, a, b):
+        r = self.walk_node(a) + self.walk_node(b) + "op " + operands[operand] + "\n"
+        self.line_number += 1
+        return r
 
-@node
-def call(expr):
-    global line_number
-    r = walk_node(expr) + "call 0\n"
-    line_number += 1
-    return r
+    @node
+    def const(self, c):
+        self.line_number += 1
+        return "loadconst " + c + "\n"
 
-@node
-def argcall(expr, args):
-    global line_number
-    s = ""
-    for i in args:
-        s += walk_node(i)
-    r = s + walk_node(expr) + "call " + str(len(args)) + "\n"
-    line_number += 1
-    return r
+    @node
+    def var(self, n):
+        self.line_number += 1
+        return "loadname " + n + "\n"
 
-@node
-def cmp(t, l, r):
-    global line_number
-    r = walk_node(l) + walk_node(r) + "cmp " + t + "\n"
-    line_number += 1
-    return r
+    @node
+    def call(self, expr):
+        r = self.walk_node(expr) + "call 0\n"
+        self.line_number += 1
+        return r
 
-@node
-def ifstmt(eval_expr, scope):
-    global line_number
-    condition = walk_node(eval_expr)
-    line_number += 1
-    s = walk_node(scope)
-    return condition + "jumpnif " + str(line_number) + "\n" + s
+    @node
+    def argcall(self, expr, args):
+        s = ""
+        for i in args:
+            s += self.walk_node(i)
+        r = s + self.walk_node(expr) + "call " + str(len(args)) + "\n"
+        self.line_number += 1
+        return r
 
-@node
-def ifelse(eval_expr, if_scope, else_scope):
-    global line_number
-    condition = walk_node(eval_expr)
-    line_number += 1
-    if_s = walk_node(if_scope)
-    line_number += 1
-    tail_of_if = line_number
-    else_s = walk_node(else_scope)
-    jump_to_else = "jumpnif " + str(tail_of_if) + "\n"
-    pass_else = "jump " + str(line_number) + "\n"
-    return condition + jump_to_else + if_s + pass_else + else_s
+    @node
+    def cmp(self, t, l, r):
+        r = self.walk_node(l) + self.walk_node(r) + "cmp " + t + "\n"
+        self.line_number += 1
+        return r
+
+    @node
+    def ifstmt(self, eval_expr, scope):
+        condition = self.walk_node(eval_expr)
+        self.line_number += 1
+        s = self.walk_node(scope)
+        return condition + "jumpnif " + str(self.line_number) + "\n" + s
+
+    @node
+    def ifelse(self, eval_expr, if_scope, else_scope):
+        condition = self.walk_node(eval_expr)
+        self.self.line_number += 1
+        if_s = self.walk_node(if_scope)
+        self.line_number += 1
+        tail_of_if = self.self.line_number
+        else_s = self.walk_node(else_scope)
+        jump_to_else = "jumpnif " + str(tail_of_if) + "\n"
+        pass_else = "jump " + str(self.self.line_number) + "\n"
+        return condition + jump_to_else + if_s + pass_else + else_s
 
 
 
